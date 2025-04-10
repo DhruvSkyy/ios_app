1b37530a7e2cb3992394bf8cdd64291caf39cf4f
Dhruv <Dhruvscools@gmail.com>
2025-04-10 18:37:33 +0100

    Initial Commit

---
+++ Cisco/CiscoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiscoApp.swift
+//  Cisco
+//
+//  Created by Dhruv Sharma on 10/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CiscoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiscoTests/CiscoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CiscoTests.swift
+//  CiscoTests
+//
+//  Created by Dhruv Sharma on 10/04/2025.
+//
+
+import Testing
+
+struct CiscoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
